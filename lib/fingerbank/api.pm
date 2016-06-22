e7d9b26d2e7b99a4bf5f332b0210874f1f0f132c
Julien Semaan <jul.semaan@gmail.com>
2016-06-22 13:50:48 -0400

    added fingerbank API doc

--- lib/fingerbank/api.pm
+++ lib/fingerbank/api.pm
@@ -1,5 +1,17 @@
 package fingerbank::api;
 
+=head1 NAME
+
+fingerbank::api
+
+=cut
+
+=head1 DESCRIPTION
+
+Module to query the Fingerbank API
+
+=cut
+
 use JSON;
 use URI;
 use LWP::UserAgent;
@@ -34,5 +46,33 @@ sub query {
 
 }
 
-1;
+=back
 
+=head1 AUTHOR
+
+Inverse inc.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
