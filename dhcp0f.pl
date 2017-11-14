dead6f6883ce303a4a53e72f25b2837c5f592198
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2017-11-14 17:09:07 -0500

    now that it's fixed, skip this whole block if we are passing json since we don't use it

--- dhcp0f.pl
+++ dhcp0f.pl
@@ -222,25 +222,25 @@ sub listen_dhcp {
         $dhcp_hash{'dst_ip'} = $l3->{'dest_ip'};
     }
 
-    foreach my $key ( keys(%{ $dhcp->{'options'} }) ) {
-        my $tmpkey = $key;
-        $tmpkey = $msg_types{$key} if ( defined( $msg_types{$key} ) );
+    if ( $output_type eq 'plain' ) {
+        foreach my $key ( keys(%{ $dhcp->{'options'} }) ) {
+            my $tmpkey = $key;
+            $tmpkey = $msg_types{$key} if ( defined( $msg_types{$key} ) );
 
-        my $output;
-        if (ref($dhcp->{'options'}{$key}) eq 'ARRAY') {
-            $output = join( ",", grep defined, @{ $dhcp->{'options'}{$key} } );
+            my $output;
+            if (ref($dhcp->{'options'}{$key}) eq 'ARRAY') {
+                $output = join( ",", grep defined, @{ $dhcp->{'options'}{$key} } );
 
-        } elsif (ref($dhcp->{'options'}{$key}) eq 'SCALAR') {
-            $output = ${$dhcp->{'options'}{$key}};
+            } elsif (ref($dhcp->{'options'}{$key}) eq 'SCALAR') {
+                $output = ${$dhcp->{'options'}{$key}};
 
-        } elsif (ref($dhcp->{'options'}{$key}) eq 'HASH') {
-            $output = Dumper($dhcp->{'options'}{$key});
+            } elsif (ref($dhcp->{'options'}{$key}) eq 'HASH') {
+                $output = Dumper($dhcp->{'options'}{$key});
 
-        } elsif (!ref($dhcp->{'options'}{$key})) {
-            $output = $dhcp->{'options'}{$key};
-        }
-        unless ( !$output ) {
-            if ( $output_type eq 'plain' ) {
+            } elsif (!ref($dhcp->{'options'}{$key})) {
+                $output = $dhcp->{'options'}{$key};
+            }
+            unless ( !$output ) {
                 $logger->info( "$tmpkey: $output" );
             }
         }
